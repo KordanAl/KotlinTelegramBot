6fcd0a3a5a7e6dec5a57486523ee0a7b9feac1c8
KordanAl <valnamekda@gmail.com>
2024-07-23 18:47:59 +0300

    KTB-04 Parse dictionary file and print contents

---
+++ src/main/kotlin/ktb_stage_1/dictionary.kt
@@ -0,0 +1,45 @@
+package ktb_stage_1
+
+import java.io.File
+import java.util.*
+
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0
+)
+
+fun main() {
+
+    val wordFile = File("words.txt")
+
+    // Тестовые данные для первого запуска
+//    wordFile.createNewFile()
+//    wordFile.writeToLowercaseToBeginning(
+//        "hello|привет|0\n" +
+//                "dog|собака|\n" +
+//                "cat|кошка|3"
+//    )
+    // наш будущий словарь
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    val lines: List<String> = wordFile.readLines()
+    for (it in lines) {
+        val line = it.split("|")
+        val original = line[0]
+        val translate = line[1]
+        val intCorrectAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0
+
+        val word = Word(original = original, translate = translate, correctAnswersCount = intCorrectAnswersCount)
+        dictionary.add(word)
+    }
+
+    dictionary.forEach{ println(it) }
+}
+
+fun File.writeToLowercaseToBeginning(text: String) {
+    val resultText = text.lowercase(Locale.getDefault())
+    val originalText = this.readText()
+
+    this.writeText(resultText + "\n" + originalText)
+}
