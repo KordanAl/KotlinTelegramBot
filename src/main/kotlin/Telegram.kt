e4e27f7c8a93cc4bec543874b63a83d28b74f33a
KordanAl <valnamekda@gmail.com>
2024-08-08 12:59:34 +0300

    KTB-12 Get telegram bot token and configure basic HTTP requests

---
+++ src/main/kotlin/Telegram.kt
@@ -0,0 +1,30 @@
+package org.example
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun main(args: Array<String>) {
+
+    val botToken = args[0]
+    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+
+
+    val client: HttpClient = HttpClient.newBuilder().build()
+    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
+    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+    val request1: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val response1: HttpResponse<String> = client.send(request1, HttpResponse.BodyHandlers.ofString())
+
+    println(response.body())
+    println(response1.body())
+
+
+
+
+
+
+}
