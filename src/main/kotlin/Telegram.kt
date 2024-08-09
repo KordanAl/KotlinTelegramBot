7be04ab23270421ec06f04595bfdf0bbc876a276
KordanAl <valnamekda@gmail.com>
2024-08-09 12:59:37 +0300

    KTB-13 I sent a request with parameters and parsed the response

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -10,23 +10,27 @@ private const val BASE_URL = "https://api.telegram.org"
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    val urlGetMe = "$BASE_URL/bot$botToken/getMe"
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+    var updateId = 0
 
+    while (true) {
+        Thread.sleep(2000)
+        val updates: String = getUpdates(botToken, updateId)
+        println(updates)
 
+        val startUpdateId = updates.lastIndexOf("update_id")
+        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
+        if (startUpdateId == -1 || endUpdateId == -1) continue
+        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
+        updateId = updateIdString.toInt() + 1
+    }
+
+}
+
+fun getUpdates(botToken: String, updateId: Int): String {
+    val urlGetUpdates = "$BASE_URL/bot$botToken/getUpdates?offset=$updateId"
     val client: HttpClient = HttpClient.newBuilder().build()
-    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
+    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
     val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
 
-    val request1: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-    val response1: HttpResponse<String> = client.send(request1, HttpResponse.BodyHandlers.ofString())
-
-    println(response.body())
-    println(response1.body())
-
-
-
-
-
-
+    return response.body()
 }
