1d000f71e86ab0158f139a8191f05c77eaa00090
KordanAl <valnamekda@gmail.com>
2024-08-09 15:27:44 +0300

    KTB-14 Parsed the string using regular expressions (RegEx)

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -6,6 +6,7 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 private const val BASE_URL = "https://api.telegram.org"
+private const val REQUEST_DELAY = 2000L
 
 fun main(args: Array<String>) {
 
@@ -13,15 +14,25 @@ fun main(args: Array<String>) {
     var updateId = 0
 
     while (true) {
-        Thread.sleep(2000)
+        Thread.sleep(REQUEST_DELAY)
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        updateId = updateIdString.toInt() + 1
+        val updateIdRegex = "\"update_id\":(\\d+?)(?:,|$)".toRegex()
+        val matchResultUpdateId: MatchResult? = updateIdRegex.find(updates)
+        val groupsUpdateId = matchResultUpdateId?.groups
+        val id: String? = groupsUpdateId?.get(1)?.value
+        if (updateId == -1) continue
+        if (id != null) {
+            updateId = id.toInt() + 1
+            println(id)
+        }
+
+        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        val matchResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = matchResult?.groups
+        val text: String? = groups?.get(1)?.value
+        if (text != null) println(text)
     }
 
 }
